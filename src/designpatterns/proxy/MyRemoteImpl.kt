d371eca4fc725b938a0a1e3f3062747178dbee18
Mauricio Barbosa <mauriiciobarbosa@gmail.com>
2019-07-22 21:54:37 -0300

    add start service method inside MyRemoteImpl file.

--- src/designpatterns/proxy/MyRemoteImpl.kt
+++ src/designpatterns/proxy/MyRemoteImpl.kt
@@ -1,5 +1,6 @@
 package designpatterns.proxy
 
+import java.rmi.Naming
 import java.rmi.server.UnicastRemoteObject
 
 class MyRemoteImpl : UnicastRemoteObject(), MyRemote {
@@ -10,5 +11,18 @@ class MyRemoteImpl : UnicastRemoteObject(), MyRemote {
 
     override fun sayHello(): String {
         return "Server says, 'Hey'"
+    }
+}
+
+fun main() {
+    try {
+        println("starting")
+        val service: MyRemote = MyRemoteImpl()
+        println("instanciated service")
+        Naming.rebind("RemoteHello", service)
+        println("Service started")
+    } catch (e: Exception) {
+        println("Error on init service")
+        e.printStackTrace()
     }
 }
--- src/designpatterns/proxy/MyRemoteTestDrive.kt
+++ src/designpatterns/proxy/MyRemoteTestDrive.kt
@@ -3,14 +3,9 @@ package designpatterns.proxy
 import java.rmi.Naming
 
 fun main() {
-    startRemote()
-
     val service = Naming.lookup("rmi://127.0.0.1/RemoteHello") as MyRemote
 
-    service.sayHello()
-}
+    val hello = service.sayHello()
 
-fun startRemote() {
-    val service: MyRemote = MyRemoteImpl()
-    Naming.rebind("RemoteHello", service)
+    println(hello)
 }
